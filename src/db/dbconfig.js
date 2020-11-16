aa4059477da326125ded03aaee2dc620c5372482
TamNhiDuong <yanxinyee@gmail.com>
2020-11-16 20:16:56 +0200

    adding pipeline

---
+++ src/db/dbconfig.js
@@ -0,0 +1,15 @@
+/*const config = {
+  user: process.env.MssqlDBUser,
+  password: process.env.MssqlDBPassword,
+  server: process.env.MssqlDBServer,
+  database: process.env.MssqlDBDatabase,
+};*/
+
+const config = {
+  user: "TestLogin",
+  password: "Test",
+  server: "softalatestvm.westeurope.cloudapp.azure.com",
+  database: "TestDB",
+ };
+
+module.exports = config;
